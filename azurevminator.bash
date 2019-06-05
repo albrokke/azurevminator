53b7beb3dbd28231b39a2da1d29e3ec2468934a1
albrokke <51419068+albrokke@users.noreply.github.com>
2019-06-05 15:26:31 -0500

    Create azurevminator.bash

---
+++ azurevminator.bash
@@ -0,0 +1,172 @@
+#!/bin/bash
+function menu {
+   clear
+   echo "**********************************************************"
+   echo "*                                                        *"
+   echo "*                Data Science VMinator v1.0              *"
+   echo "*                                                        *"
+   echo "**********************************************************"
+   echo
+
+   echo
+   echo "  Enter a to authenticate to Azure"
+   echo "  Enter l to list your team's VMs"
+   echo "  Enter d to deploy a new VM"
+   echo "  Enter r to remove an existing VM"
+   echo "  Enter c to connect a VM"
+   echo "  Enter e to exit"
+   read ACTION
+   case $ACTION in
+
+       a)
+          auth
+       ;;
+       l)
+               echo
+               echo "    Here are Your team's deployed VMs:"
+               echo
+               az vm list -g walledgarden -o table
+               echo "    Press [Enter] to continue"
+               read CONTINUE
+               menu
+        ;;
+       d)
+             deploy
+        ;;
+       r)
+             remove
+        ;;
+       c)
+             connect
+        ;;
+       e)
+             exit
+        ;;
+   esac
+}
+
+function auth {
+    az login
+}
+
+function deploy {
+    VMNAME="$USER$(date +%s)"
+    RGNAME='walledgarden'
+    LOCATION='northcentralus'  # e.g. westus2
+    PUB='microsoft-ads'
+    OFFER='linux-data-science-vm-ubuntu'
+    SKU='linuxdsvmubuntu'
+    VERSION='latest'
+    VNET='walledgarden'
+    SUBNET='resources'
+ 
+    echo
+    echo "   Enter the number of the virtual machine size you would like to deploy "
+    echo "     ________________________________________________________________"
+    echo "    |Choice   |Size   |CPU    |GPU    |RAM    |Estimated Monthly Cost|"
+    echo "    |1                |DS2V2  | 2     |0      |  7GB  |$  108.62"
+    echo "    |2                |DS3V2  | 4     |0      | 14GB  |$  217.00"
+    echo "    |3                |DS4V2  | 8     |0      | 28GB  |$  8##.##"
+    echo "    |4                |NC6    | 6     |1      | 56GB  |$  791.32"
+    echo "    |5                |NC12   |12     |2      |112GB  |$ 1582.64"
+    echo ""
+    read CONFIG
+    case $CONFIG in
+        1)
+                 SIZE='Standard_DS2_v2'
+             ;;
+        2)
+                 SIZE='Standard_DS3_v2'
+             ;;
+        3)
+                 SIZE='Standard_DS4_v2'
+             ;;
+        4)
+                 SIZE='Standard_NC6'
+             ;;
+        5)
+                 SIZE='Standard_NC12'
+             ;;
+  esac
+
+  echo
+  echo "    Creating Azure VM deployment.  This can take up to 5 minutes, please wait..."
+  echo
+
+  az vm create \
+    --name $VMNAME --resource-group $RGNAME --image $PUB\:$OFFER\:$SKU\:$VERSION \
+    --plan-name $SKU --plan-product $OFFER --plan-publisher $PUB \
+    --admin-username $USER \
+    --generate-ssh-keys \
+    --vnet-name $VNET \
+    --subnet $SUBNET \
+    --public-ip-address ""
+
+  echo
+  sleep 5
+  menu
+}
+
+function remove {
+    RGNAME='walledgarden'
+    declare -a DVMS
+    declare -a RESOURCES
+    declare -i dcounter
+    declare -i DELETEVM
+    delimiter="\""
+    dcounter=0
+    counter=0
+    DVMS=($(az vm list --resource-group $RGNAME --query "[].{name:name}" -o table | tr '\n' ' '))
+    for DVM in "${DVMS[@]}"i
+        do
+            echo "$dcounter : $DVM"
+                      dcounter=`expr $dcounter + 1`
+        done
+    echo
+    echo "    Enter the number of the VM you would like to delete"
+    echo
+    read DNUM
+    echo
+    echo "    Submitting VM deletion to Azure this can take up to 5 minutes."
+    echo "az vm delete --resource-group $RGNAME --name ${DVMS[$DNUM]} --yes"
+    az vm delete --resource-group $RGNAME --name ${DVMS[$DNUM]} --yes
+    RESOURCES=($(az resource list -g walledgarden --query "[].{Name:name,Type:type}" -o table))
+    for RESOURCE in "${RESOURCES[@]}"
+        do
+            counter=`expr $counter + 1`
+            if [[ $RESOURCE == *${DVMS[$DNUM]}* ]]; then
+              az resource delete -g $RGNAME -n $RESOURCE --resource-type ${RESOURCES[counter]}
+            fi
+        done
+
+    sleep 20
+    menu
+}
+
+function connect {
+    RGNAME="walledgarden"
+    declare -a VMS
+    declare -i counter
+    declare -i ACCESS
+    delimiter="\""
+    counter=0
+    VMS=($(az vm list --resource-group $RGNAME --query "[].{name:name}" -o table | tr '\n' ' ' ))
+    for VM in "${VMS[@]}"
+        do
+            echo "$counter : $VM"
+            counter=`expr $counter + 1`
+        done
+
+    echo
+    echo "    Enter the number of the VM you would like to connect to"
+    echo
+
+    read ACCESS
+    NIC="${VMS[$ACCESS]}VMNic"
+    IPDATA="$(az vm nic show --resource-group $RGNAME --vm-name ${VMS[$ACCESS]} --nic $NIC | grep "privateIpAddress\":" | tr '\"privateIpAddress\":\"' ' ' | tr '\",' ' ')"
+    ssh $IPDATA
+    menu
+}
+
+menu
+exit
